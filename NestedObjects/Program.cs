510db3e6fbd3076921b0a02085bbaa2370cfb6ff
ankit <ankit@DESKTOP-8U9OHRT>
2023-05-16 11:50:41 +0530

    changes done

--- NestedObjects/Program.cs
+++ NestedObjects/Program.cs
@@ -4,29 +4,30 @@ using System.Reflection;
 
 namespace NestedObjects
 {
-   public class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            
+
             /*Tests*/
             JObject obj = JObject.Parse("{\"a\":{\"b\":{\"c\":\"d\"}}}");
-             GetNestedValueByKey(obj, "a/b/c");
-           // Console.WriteLine(value); // Output: "d"
+            var value = GetNestedValueByKey(obj, "a/b/c");
+            Console.WriteLine(value); // Output: "d"
             obj = JObject.Parse("{\"x\":{\"y\":{\"z\":\"a\"}}}");
-            GetNestedValueByKey(obj, "x/y");
-            //Console.WriteLine(value); // Output: { "z": "a" }
+            value = GetNestedValueByKey(obj, "x/y");
+            Console.WriteLine(value); // Output: { "z": "a" }
             obj = JObject.Parse("{\"x\":{\"y\":{\"z\":\"a\"}}}");
-            GetNestedValueByKey(obj, "x");
-            //Console.WriteLine(value); // Output: { "y": { "z": "a" } }
+            value = GetNestedValueByKey(obj, "x");
+            Console.WriteLine(value); // Output: { "y": { "z": "a" } }
             obj = JObject.Parse("{\"x\":{\"y\":{\"z\":\"a\"}}}");
-            GetNestedValueByKey(obj, "k"); // key not exists in json
-           // Console.WriteLine(value); // Output: Key k not exists
+            value = GetNestedValueByKey(obj, "k"); // key not exists in json
+            Console.WriteLine(value); // Output: Key k not exists
         }
 
 
-        public static void GetNestedValueByKey(JObject obj, string key)
+        public static object GetNestedValueByKey(JObject obj, string key)
         {
+
             var keys = key.Split('/');
             JToken current = obj;
             foreach (var k in keys)
@@ -34,11 +35,11 @@ namespace NestedObjects
                 current = current[k];
                 if (current == null)
                 {
-                    current= $"Key {key} not exists";
+                    current = $"Key {key} not exists";
                 }
             }
-            //return current;
-            Console.WriteLine(current);
+            return current;
+
         }
     }
 }
--- NestedUnitTest/UnitTest1.cs
+++ NestedUnitTest/UnitTest1.cs
@@ -12,21 +12,27 @@ namespace NestedObjects
         public static void Test1()
         {
             JObject obj = JObject.Parse("{\"a\":{\"b\":{\"c\":\"d\"}}}");
-             Program.GetNestedValueByKey(obj, "a/b/c");
+            var value = Program.GetNestedValueByKey(obj, "a/b/c");
+            Assert.Equal("d",value.ToString());
         }
 
         [Fact]
         public static void Test2()
         {
-            JObject obj = JObject.Parse("{\"x\":{\"y\":{\"z\":\"a\"}}}");
-            Program.GetNestedValueByKey(obj, "x/y");
+            JObject obj = JObject.Parse("{\"a\":{\"b\":{\"c\":\"d\"}}}");
+            var value = Program.GetNestedValueByKey(obj, "k");
+            Assert.Equal("Key k not exists", value.ToString());
         }
+
 
         [Fact]
         public static void Test3()
         {
-            JObject obj = JObject.Parse("{\"x\":{\"y\":{\"z\":\"a\"}}}");
-             Program.GetNestedValueByKey(obj, "x");
+            JObject obj = JObject.Parse("{\"a\":{\"b\":{\"c\":\"d\"}}}");
+            var value = Program.GetNestedValueByKey(obj, "k");
+            Assert.Equal("Key k", value.ToString());
         }
+
+
     }
 }
